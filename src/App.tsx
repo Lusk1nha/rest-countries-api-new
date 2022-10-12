de00d30b13196f2c63ad269fa577f40537f6ebc4
unknown <lucaspedro517@gmail.com>
2022-10-11 22:11:51 -0300

    Creating theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { base, light, dark } from "./styles/themes";
 
-function App() {
+const themesMap: any = {
+  light,
+  dark
+};
+
+export default function App() {
+  const [currentTheme, setCurrentTheme] = useState<string>('light');
+  const theme = { ...base, colors: themesMap[currentTheme] };
+  
+  useEffect(() => {
+
+  }, [currentTheme])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        
+      </ThemeProvider>
     </div>
   );
 }
-
-export default App;
---
+++ src/styles/themes.ts
@@ -0,0 +1,26 @@
+export const base = {
+   breakpoints: [],
+   space: [],
+   fonts: {
+      heading: '',
+      body: '',
+      footer: ''
+   },
+   fontSize: []
+};
+
+
+export const light = {
+   bgColor: 'hsl(0, 0%, 98%)',
+   elementsColor: 'hsl(0, 0%, 100%)',
+   textColor: 'hsl(200, 15%, 8%)',
+   inputColor: 'hsl(0, 0%, 52%)'
+};
+
+
+export const dark = {
+   bgColor: 'hsl(207, 26%, 17%)',
+   elementsColor: 'hsl(209, 23%, 22%)',
+   textColor: 'hsl(0, 0%, 100%)',
+   inputColor: 'hsl(0, 0%, 52%)'
+};
